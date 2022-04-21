8edf42800a87f63f5c47cd40cc9f614885f94195
Rikey Chen <rikey.chen16@gmail.com>
2022-04-20 18:45:17 -0700

    DEV-2: Connect MongoDB Atlas

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { mongoURI } from './config/keys.js';
 
 const app = express();
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
