9f8ee4358a0e6acda09441496bf60fa8edd7790e
Rikey Chen <rikey.chen16@gmail.com>
2022-05-04 14:04:33 -0700

    BDEV3: Fix code formatting

--- loaders/express.js
+++ loaders/express.js
@@ -2,17 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import config from '../config';
 import morganBody from 'morgan-body';
-import useAllRoutes from '../api/routes/routes';
+import useAllRoutes from '../api/routes';
 
 export default async () => {
-    const app = express();
+  const app = express();
 
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    morganBody(app);
-    useAllRoutes(app);
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  morganBody(app);
+  useAllRoutes(app);
 
-    app.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
+  app.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
 
-    return app;
+  return app;
 }
