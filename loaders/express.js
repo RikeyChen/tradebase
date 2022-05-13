eef11836667e168e16f327ffc1f61a8fb1210515
Rikey Chen <rikey.chen16@gmail.com>
2022-05-13 14:11:57 -0700

    BDEV3: Add passport middleware

--- loaders/express.js
+++ loaders/express.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import config from '../config';
 import morganBody from 'morgan-body';
+import passport from 'passport';
+import { passportConfig } from '../middleware/passport';
+import config from '../config';
 import useAllRoutes from '../api/routes';
 
 export default async () => {
@@ -9,7 +11,10 @@ export default async () => {
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  app.use(passport.initialize());
+
   morganBody(app);
+  passportConfig(passport);
   useAllRoutes(app);
 
   app.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
